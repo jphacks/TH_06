8166b795fb77bdf155063665ff30b5b8f83e041f
fujiwara toshiki <0tn0mb01726530u@ezweb.ne.jp>
2015-11-29 13:17:21 +0900

    test

---
+++ MecaWanco/Assets/CameraMove.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour
+{
+    public GameObject Player;
+    float spase = 3.0f;
+
+    public GameObject prefab; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //キャラを中心にカメラを移動
+        transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y + 2, -10);
+        
+        //画面端まで行ったらストップ
+        if (transform.position.x < -28)
+        {
+            transform.position = new Vector3(-28,Player.transform.position.y+2, -10);
+        }
+        if (transform.position.x >= 36)
+        {
+            transform.position = new Vector3(36, Player.transform.position.y+2, -10);
+        }
+
+	}
+
+    //----------------------------------
+    //機能：ユニットの移動
+    //引数：なし
+    //戻値：なし
+    //----------------------------------
+    void OnCollisionEnter(Collision target)
+    {
+        if (target.gameObject.tag == "Wall")
+        {
+
+        }
+    }
+
+}
---
+++ MecaWanco/Assets/Unit_Action.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit_Action : MonoBehaviour {
+
+    //--------------------------------
+    public float speed;
+    public float jump;
+
+    public LayerMask layer;
+    public GameObject bulletP;
+    //--------------------------------
+    bool isGroundFit = false;
+    bool isWallFit = false;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        move();
+        Attack();
+	}
+
+    //----------------------------------
+    //機能：ユニットの移動
+    //引数：なし
+    //戻値：なし
+    //----------------------------------
+    void move()
+    {
+      
+        Vector2 move = new Vector2(speed, 0);
+        move.x = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime * 150;
+        move.y = GetComponent<Rigidbody2D>().velocity.y;
+        //move *= speed;
+        if(isWallFit)
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, move.y);
+        else
+        GetComponent<Rigidbody2D>().velocity = move;
+
+        ////振り返り
+        //if (move.x > 0)
+        //{
+        //    isRightWallFit = Physics2D.Linecast(
+        //     transform.position,
+        //     transform.position + new Vector3(0.18f * 2, 0, 0),
+        //     wall);
+        //}
+        //else if (move.x < 0)
+        //{
+        //    isLeftWallFit = Physics2D.Linecast(
+        //      transform.position,
+        //      transform.position + new Vector3(-0.18f * 2, 0, 0),
+        //      wall);
+        //}
+       
+
+        //ジャンプ
+        isGroundFit = Physics2D.Linecast(
+            transform.position,
+            transform.position + new Vector3(-0.18f * 2, -0.18f * 2, 0),
+            layer);
+
+  
+        if (Input.GetButtonDown("Jump") && isGroundFit)
+        {
+            print("jump!");
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(move.x, jump));
+        }
+
+    }
+    //----------------------------------
+    //機能：ユニットの攻撃
+    //引数：なし
+    //戻値：なし
+    //----------------------------------
+    void Attack()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+
+        }
+    }
+
+    //----------------------------------
+    //機能：何かに当たったら呼ばれる関数
+    //引数：なし
+    //戻値：なし
+    //----------------------------------
+    void OnCollisionEnter(Collision2D target)
+    {
+    }
+}
